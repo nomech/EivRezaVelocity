fe074b9f0601db516c409fe5b329d5ba00cef990
nomech <mech1803@gmail.com>
2024-10-31 19:08:34 +0100

    Added form images

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,33 @@
 const customData = [
-  { option: "Fuel type", answer: ["Petrol", "Diesel", "Electric", "Hybrid"], selected: null },
-  { option: "Engine size", answer: ["350hp", "450hp", "600hp"],selected: null },
-  { option: "Transmission", answer: ["Automatic", "Manual"],selected: null },
-  { option: "Color", answer: ["Red", "Blue", "Black", "White"],selected: null  },
+  {
+    option: "Fuel type",
+    answer: ["Petrol", "Diesel", "Electric", "Hybrid"],
+    image: "../assets/images/step1.png",
+    selected: null,
+  },
+  {
+    option: "Engine size",
+    answer: ["350hp", "450hp", "600hp"],
+    image: "../assets/images/step2.png",
+    selected: null,
+  },
+  {
+    option: "Transmission",
+    answer: ["Automatic", "Manual"],
+    image: "../assets/images/step3.png",
+    selected: null,
+  },
+  {
+    option: "Color",
+    answer: ["Red", "Blue", "Black", "White"],
+    image: "../assets/images/step4.png",
+    selected: null,
+  },
   {
     option: "Interior",
     answer: ["White lather", "Red Synthetic leather", "Black fabric"],
+    image: "../assets/images/step5.png",
+    selected: null,
   },
 ];
 
@@ -35,81 +57,80 @@ const createLabelElement = (className, name, text) => {
 
 const orderForm = document.querySelector(".order-section__form");
 const option = document.createElement("h3");
-
+const orderImage = document.querySelector(".order-section__image");
+orderImage.src = customData[currentQuestion].image;
 option.innerText = customData[currentQuestion].option;
 option.className = "order-section__option";
 orderForm.appendChild(option);
 
 customData[currentQuestion].answer.forEach((data, index) => {
   const inputField = createInputElement(
-    "quiz__input",
+    "order__input",
     "radio",
     "answer",
     index,
     data
   );
 
-  const label = createLabelElement("quiz__label", data, data);
+  const label = createLabelElement("order__label", data, data);
 
   orderForm.appendChild(inputField);
   orderForm.appendChild(label);
 });
 
 nextButton.addEventListener("click", () => {
-    customData[currentQuestion].selected = document.querySelector('input[name="answer"]:checked').value;
-    console.log(customData)
+  customData[currentQuestion].selected = document.querySelector(
+    'input[name="answer"]:checked'
+  ).value;
 
+  currentQuestion++;
+  orderImage.src = customData[currentQuestion].image;
+  orderForm.innerHTML = "";
 
-    currentQuestion++;
-    orderForm.innerHTML = "";
+  option.innerText = customData[currentQuestion].option;
+  orderForm.appendChild(option);
 
-    option.innerText = customData[currentQuestion].option;
-    orderForm.appendChild(option);
+  customData[currentQuestion].answer.forEach((data, index) => {
+    const inputField = createInputElement(
+      "order__input",
+      "radio",
+      "answer",
+      index,
+      data
+    );
 
-    customData[currentQuestion].answer.forEach((data, index) => {
-      const inputField = createInputElement(
-        "quiz__input",
-        "radio",
-        "answer",
-        index,
-        data
-      );
+    const label = createLabelElement("order__label", data, data);
 
-      const label = createLabelElement("quiz__label", data, data);
-
-      orderForm.appendChild(inputField);
-      orderForm.appendChild(label);
-    });
-    if (customData.length - 1 === currentQuestion) {
-        nextButton.addEventListener("click", submitAnswer);
-    
-      }  
+    orderForm.appendChild(inputField);
+    orderForm.appendChild(label);
+  });
+  if (customData.length - 1 === currentQuestion) {
+    nextButton.addEventListener("click", submitAnswer);
+  }
 });
 
-
 const submitAnswer = () => {
-    const orderSection = document.querySelector('.order-section');
-    orderSection.innerHTML = '';
-    const orderSummary = document.createElement('div');
-    orderSummary.className = 'order-section__summary';
-    orderSection.appendChild(orderSummary);
-    customData.forEach((data) => {
-        const selectedAnswer = document.createElement('p');
-        selectedAnswer.className = 'order-section__answer';
-        selectedAnswer.innerText = `${data.answer[data.selected]}`;
-        
-        const selectedTitle = document.createElement('h3');
-        selectedTitle.className = 'order-section__option';
-        selectedTitle.innerText = data.option;
-        orderSummary.appendChild(selectedTitle);
-        orderSummary.appendChild(selectedAnswer);
-    });
-    const orderButton = document.createElement('button');
-    orderButton.className = 'button button--submit';
-    orderButton.innerText = 'Order';
-    orderButton.addEventListener('click', () => {
-        alert('Order has been placed');
-    });
-    orderSection.appendChild(orderButton);
+  const orderSection = document.querySelector(".order-section");
+  orderSection.innerHTML = "";
+  const orderSummary = document.createElement("div");
+  orderSummary.className = "order-section__summary";
+  orderSection.appendChild(orderSummary);
+  customData.forEach((data) => {
+    const selectedAnswer = document.createElement("p");
+    selectedAnswer.className = "order-section__answer";
+    selectedAnswer.innerText = `${data.answer[data.selected]}`;
 
+    const selectedTitle = document.createElement("h3");
+    selectedTitle.className = "order-section__option";
+    selectedTitle.innerText = data.option;
+    orderSummary.appendChild(selectedTitle);
+    orderSummary.appendChild(selectedAnswer);
+  });
+  const orderButton = document.createElement("button");
+  orderButton.className = "button button--submit";
+  orderButton.innerText = "Order";
+  orderButton.addEventListener("click", () => {
+    alert("Order has been placed");
+  });
+  orderSection.appendChild(orderButton);
 };
